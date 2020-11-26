9d450aad20a0a822b226a9dff8c284bf141792fc
ike <2740262535@qq.com>
2020-11-26 08:55:37 +0800

    deploy

---
+++ precache-manifest.bea9eecfd53b30223fa270af90fb1637.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f192d8c7cecba983e093",
+    "url": "/morney-success-01-website/css/app.266aeedc.css"
+  },
+  {
+    "revision": "bb442021e5aad562c15a7d2d486bc456",
+    "url": "/morney-success-01-website/index.html"
+  },
+  {
+    "revision": "f192d8c7cecba983e093",
+    "url": "/morney-success-01-website/js/app.84a0a56d.js"
+  },
+  {
+    "revision": "ec740be7f6ba3402d604",
+    "url": "/morney-success-01-website/js/chunk-vendors.6e2ad75e.js"
+  },
+  {
+    "revision": "f3aaee380deddea3de78c3de33e81ce5",
+    "url": "/morney-success-01-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-success-01-website/robots.txt"
+  }
+]);
